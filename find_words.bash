8aa259a0dbfa4b7ce48ae05fc76796d1bd6f7c18
André Luiz Clinio <clinio@tecgraf.puc-rio.br>
2024-10-03 17:17:09 -0300

    adj: script

--- find_words.bash
+++ find_words.bash
@@ -4,12 +4,13 @@ for word in "$@"
 do
   path=`find . -name "$word" -type d`
   pt=$path/pt.txt
+  repo=`dirname $path`
   if [ -e $pt ]; then
      text=`more $pt`
   else 
      text="??"
   fi
-  echo $word - $text
+  echo "$word ($repo) - $text"
 done
 
 
