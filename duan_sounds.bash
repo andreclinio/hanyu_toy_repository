e5d1fa54236895963e87054d4555c10773cdefa9
André Luiz Clinio <clinio@tecgraf.puc-rio.br>
2025-04-09 16:20:33 -0300

    ren

--- duan_sounds.bash
+++ duan_sounds.bash
@@ -7,17 +7,17 @@ n=1
 for dir in `find repository -mindepth 2 -maxdepth 2 -type d` 
 do
     hanzi=`basename $dir`
-    file=$dir/sounds/duan/$hanzi.mp3
+    file=$dir/sounds/male/$hanzi.mp3
     if [ -e $file ]; then
        echo "$n: já existe! ($file)"
        continue;
     fi
     # dir="repository/$level/$hanzi"
-    mkdir -p $dir/sounds/duan
+    mkdir -p $dir/sounds/male
     echo "$n: call enhanced_google_sound.py for $hanzi (file: $file)..."
 
     ./venv/bin/python enhanced_google_sound.py $hanzi
-    mv duan.mp3 $file
+    mv male.mp3 $file
     echo "$n: OK ($dir $n)"
     n=$((n+1))
 done
