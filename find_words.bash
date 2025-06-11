c0ea4c485dfcd8349fc4a2e47a205663f3fd77f4
André Luiz Clinio <clinio@tecgraf.puc-rio.br>
2025-06-11 19:53:13 -0300

    mais palavras

--- find_words.bash
+++ find_words.bash
@@ -5,13 +5,19 @@ do
   word=`basename $dir`
   path=`find . -name "$word" -type d`
   pt=$path/pt.txt
+  py=$path/pinyin.txt
   repo=`dirname $path`
   if [ -e $pt ]; then
      text=`more $pt`
   else 
      text="??"
   fi
-  echo "$word ($repo/$word) - $text"
+  if [ -e $py ]; then
+     pinyin=`more $py`
+  else 
+     pinyin="??"
+  fi
+  echo "$word ($repo/$word) - $text [$pinyin]"
 done
 
 
