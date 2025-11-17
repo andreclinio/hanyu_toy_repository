396d9d807762def2f8581f8d7513f01c335b6df5
André Luiz Clinio <clinio@tecgraf.puc-rio.br>
2025-11-17 20:44:36 -0300

    lic 9 e 10 HSK3 (pt I)

--- find_words.bash
+++ find_words.bash
@@ -5,19 +5,25 @@ do
   word=`basename $dir`
   path=`find . -name "$word" -type d`
   pt=$path/pt.txt
+  en=$path/en.txt
   py=$path/pinyin.txt
   repo=`dirname $path`
-  if [ -e $pt ]; then
-     text=`more $pt`
+  if [ -e $en ]; then
+     text_en=`more $en`
   else 
-     text="??"
+     text_en="??"
+  fi
+  if [ -e $pt ]; then
+     text_pt=`more $pt`
+  else 
+     text_pt="??"
   fi
   if [ -e $py ]; then
      pinyin=`more $py`
   else 
      pinyin="??"
   fi
-  echo "$word ($repo/$word) - $text [$pinyin]"
+  echo "$word ($repo/$word) - $text_pt - $text_en [$pinyin]"
 done
 
 
