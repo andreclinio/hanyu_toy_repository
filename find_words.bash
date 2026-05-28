3fb9185143d2873b499d9a9460b3e3affc190837
André Luiz Clinio <clinio@tecgraf.puc-rio.br>
2026-05-28 11:32:58 -0300

    mais duas palavras

--- find_words.bash
+++ find_words.bash
@@ -1,24 +1,29 @@
-# /bin/bash
+#!/bin/bash
 
 for dir in `find repository -mindepth 2 -maxdepth 2 -type d`
 do
+  # Check for spaces
+  if [[ "$dir" =~ [[:space:]] ]]; then
+      echo "Warning: '$dir' contains spaces."
+      exit 1;
+  fi
   word=`basename $dir`
   path=`find . -name "$word" -type d`
   pt=$path/pt.txt
   en=$path/en.txt
   py=$path/pinyin.txt
   repo=`dirname $path`
-  if [ -e $en ]; then
+  if [ -e "$en" ]; then
      text_en=`more $en`
   else 
      text_en="??"
   fi
-  if [ -e $pt ]; then
+  if [ -e "$pt" ]; then
      text_pt=`more $pt`
   else 
      text_pt="??"
   fi
-  if [ -e $py ]; then
+  if [ -e "$py" ]; then
      pinyin=`more $py`
   else 
      pinyin="??"
